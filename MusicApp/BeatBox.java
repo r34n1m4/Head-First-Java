f9370bcb4f36f8e475b2c556a0a076699ad4c04b
Vadim <65947614+r34n1m4@users.noreply.github.com>
2022-09-18 23:04:36 +0300

    Add files via upload

--- MusicApp/BeatBox.java
+++ MusicApp/BeatBox.java
@@ -3,6 +3,7 @@ package MusicApp;
 import java.awt.*;
 import javax.swing.*;
 import javax.sound.midi.*;
+import java.io.*;
 import java.util.*;
 import java.awt.event.*;
 
@@ -176,6 +177,50 @@ public class BeatBox {
 
         } catch(Exception e) {e.printStackTrace(); }
         return event;
+    }
+
+    public class MySendListener implements ActionListener {
+        public void actionPerformed(ActionEvent a) {
+            boolean[] checkboxState = new boolean[256];
+            for (int i = 0; i < 256; i++) {
+                JCheckBox check = (JCheckBox) checkboxList.get(i);
+                if (check.isSelected()) {
+                    checkboxState[i] = true;
+                }
+            }
+            try {
+                FileOutputStream fileStream = new FileOutputStream(new File("Checkbox.ser"));
+                ObjectOutputStream os = new ObjectOutputStream(fileStream);
+                os.writeObject(checkboxState);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public class MyReadListener implements ActionListener {
+        public void actionPerformed(ActionEvent a) {
+            boolean[] checkboxState = null;
+            try {
+                FileInputStream fileIn = new FileInputStream(new File("Checkbox.ser"));
+                ObjectInputStream is = new ObjectInputStream(fileIn);
+                checkboxState = (boolean[]) is.readObject();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            for (int i = 0; i < 256; i++) {
+                JCheckBox check = (JCheckBox) checkboxList.get(i);
+                if (checkboxState[i]) {
+                    check.setSelected(true);
+                } else {
+                    check.setSelected(false);
+                }
+            }
+
+            sequencer.stop();
+            buildTrackAndStart();
+        }
     }
 
 } // close class
