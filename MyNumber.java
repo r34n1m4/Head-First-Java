735ec2632668f4c3273983532bc7625224351a96
Vadim <65947614+r34n1m4@users.noreply.github.com>
2022-10-07 10:55:48 +0300

    Add files via upload

---
+++ MyNumber.java
@@ -0,0 +1,50 @@
+package com.reanima;
+
+public class MyNumber {
+    private int number;
+    private int sumOfNumbers;
+
+    public MyNumber(int number) {
+        this.number = number;
+        this.sumOfNumbers = sumOfNumbers;
+    }
+
+    public boolean isPrime() {
+
+        if (number < 2 ) {
+            return false;
+        }
+        for (int i = 2; i <= number - 1; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int sumUptoN() {
+        for (int i = 1; i <= number; i++) {
+            sumOfNumbers += i;
+        }
+        return sumOfNumbers;
+    }
+
+    public int sumOfDivisors() {
+        int sumOfDividers = 0;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0 ) {
+                sumOfDividers += i;
+            }
+        }
+        return sumOfDividers;
+    }
+
+    public void numberTriangle() {
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ MyNumberRunner.java
@@ -0,0 +1,19 @@
+package com.reanima;
+
+import java.util.Scanner;
+
+public class MyNumberRunner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int n = scanner.nextInt();
+
+        MyNumber number = new MyNumber(n);
+        boolean isPrime = number.isPrime();
+        System.out.println("Is prime? " + isPrime);
+
+        System.out.println("Sum of numbers: " + number.sumUptoN());
+        System.out.println("Sum of divisors: " + number.sumOfDivisors());
+        number.numberTriangle();
+    }
+}
