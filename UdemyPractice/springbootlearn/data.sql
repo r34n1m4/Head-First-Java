5e98ef12216a506cab9b7a60c94b458011bf9aa1
Vadim <65947614+r34n1m4@users.noreply.github.com>
2022-10-25 21:32:53 +0300

    Add files via upload

---
+++ UdemyPractice/springbootlearn/data.sql
@@ -0,0 +1,11 @@
+insert into COURSE (ID, AUTHOR, NAME)
+values (100001, 'in28minutes', 'Learn Microservices');
+
+insert into COURSE (ID, AUTHOR, NAME)
+values (123456, 'in28minutes', 'Learn Java');
+
+insert into COURSE (ID, AUTHOR, NAME)
+values (123, 'K.Sierra', 'Head First JAVA');
+
+insert into COURSE (ID, AUTHOR, NAME)
+values (1, 'Google', 'How to sleep at work');
